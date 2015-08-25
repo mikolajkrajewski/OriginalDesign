799b61bbe1f055b8ed2d51ec64202b53c79df6f8
unknown <mikrajewski@697-334-Edoras.sf.k12.internal>
2015-08-25 11:36:56 -0700

    Kezar Track v1

--- build-tmp/source/OriginalDesign.java
+++ build-tmp/source/OriginalDesign.java
@@ -1,12 +1,28 @@
-void setup()
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class OriginalDesign extends PApplet {
+
+public void setup()
 {
   size(700,600);
 }
-void draw()
+public void draw()
 {
   stadium();
 }
-void stadium()
+public void stadium()
 {
   background(150);
   noStroke();
@@ -19,7 +35,7 @@ void stadium()
   rect(0,553,700,7);
   kezarTrack();
 }
-void kezarTrack()
+public void kezarTrack()
 {
   smooth();
   noStroke();
@@ -50,4 +66,13 @@ void kezarTrack()
   rect(200,230,300,140);
   
   
+}
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "OriginalDesign" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
 }
